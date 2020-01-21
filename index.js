7202b640267b029cecb56fbed404ce3e506dc72b
FernandaVera <dinosauriosnaranja@gmail.com>
2020-01-21 16:01:42 -0600

    ejercicio

---
+++ index.js
@@ -0,0 +1,64 @@
+//Create Graph Class
+class Graph{
+    //defining vertex array and adjacent list
+    constructor(noOfVertices){
+        this.noOfVertices = noOfVertices;
+        this.AdjList = new Map();
+    }
+//G = (V,E)
+//add Vertex to the graph
+    addVertex(v){
+        //intialize the adjacent list with a null array
+        this.AdjList.set(v, []);
+    }
+//Add edge to the graph
+    addEdge(v,w){
+        //get the list for the vertex v and put the vertex w denoting edge between v and w
+        this.AdjList.get(v).push(w);
+        //Since the graph is indirected, add an edge from w to v also (direccional)
+        this.AdjList.get(w).push(v);
+    }
+//Print the vertex and ADJACENCY LIST
+    printGraph(){
+        //get all the vertex
+        var get_keys = this.AdjList.keys();
+        //[{"A",[{"A","B"},{"A","C"}]}, {"B",[{"B","A"}]}, {"C",[{"C","A"}]}, {"D",[]}, {"E",[]}, {"F",[]}]
+        //["A","B",...]
+        //iterate over the vertex
+        for(var i of get_keys){
+            //get the corresponding, adjacency list for the vertex
+            var get_values = this.AdjList.get(i);
+            //Primera Iteración var get_values = this.AdjList.get("A");
+            //Segunda Iteración var get_values = this.AdjList.get("B");
+            var conc = "";
+            //iterating over the adjacency list concetenate the values into a string
+            for(var j of get_values) conc +=j + " ";
+            //print tthe vertex and its adjacency list
+            console.log(i + " -> " + conc);
+        }
+    }
+}
+//Using the above implemented graph class
+var g = new Graph(9);
+var vertices = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I' ];
+//adding vertices
+for(var i = 0; i< vertices.length; i++){
+    g.addVertex(vertices[i]);
+}
+//adding edges
+g.addEdge('A', 'B');
+g.addEdge('A', 'D');
+g.addEdge('A', 'G');
+g.addEdge('B', 'I');
+g.addEdge('B', 'C');
+g.addEdge('B', 'E');
+g.addEdge('B', 'A');
+g.addEdge('C', 'F');
+g.addEdge('C', 'I');
+g.addEdge('D', 'G');
+g.addEdge('E', 'H');
+g.addEdge('E', 'G');
+g.addEdge('F', 'I');
+g.addEdge('G', 'H');
+g.addEdge('H', 'I');
+g.printGraph();
